4a7ecd48e12bb6e8adc8e6e0decdec37efbbb7ff
Marcos Dimitris Fetcu <Marcos-Dimitris.Fetcu@endava.com>
2025-08-12 23:48:14 +0300

    update base URL to use Azure backend v2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:8000";
-const BASE_URL = "math-backend-app.salmonstone-a26147bd.eastus.azurecontainerapps.io";
+const BASE_URL = "http://math-backend-app.salmonstone-a26147bd.eastus.azurecontainerapps.io";
 
 async function calculatePow() {
   const base = document.getElementById("pow-base").value;
